5b1e8a2d4dbcce3af01f022b34e00c7676689d9d
Rion-Suzuki <r.suzuki@kimura-lab.net>
2023-06-18 14:28:54 +0900

    Update 1_release_land.py

--- sequence/1_release_land.py
+++ sequence/1_release_land.py
@@ -160,11 +160,11 @@ if __name__ == '__main__':
                 break
     except KeyboardInterrupt:
        print("Program interrupted")
-        sys.exit(0)
+    sys.exit(0)
 
     #####着地判定######
     try:
-        while 1:
+         while 1:
             press_count_land, press_judge_land = pressdetect_land(0.1) #閾値0.1
             gps_count_land, gps_judge_land = gpsdetect_land(10) #閾値10
             acc_count_land, acc_judge_land = acc_detect_land(0.05) # 加速度による着地判定（閾値0.05）
@@ -180,7 +180,7 @@ if __name__ == '__main__':
              #if press_judge_land == 1:
 #                 print('land detected')
 #                 break
-    except KeyboardInterrupt:
+except KeyboardInterrupt:
         print("Program interrupted")
         sys.exit(0)
 
