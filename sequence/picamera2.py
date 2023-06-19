3906a240ef0be5c0d5adca0823fd5e7fcfa349fe
hayaocci <7319079@ed.tus.ac.jp>
2023-06-19 18:55:59 +0900

    Create picamera2.py

---
+++ sequence/picamera2.py
@@ -0,0 +1,14 @@
+import picamera2 as picamera
+import time
+import traceback
+import os
+
+camera = Picamera2()
+
+camera_config = camera.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
+            camera.configure(camera_config)
+            camera.start()
+            time.sleep(2)
+            camera.capture_file("sample.jpg")
+
+print("done")
