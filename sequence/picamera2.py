4f637dfc7e0058de5e5007724df3e8ab9df27834
hayaocci <7319079@ed.tus.ac.jp>
2023-06-19 19:01:05 +0900

    Update picamera2.py

--- sequence/picamera2.py
+++ sequence/picamera2.py
@@ -6,10 +6,9 @@ import os
 camera = Picamera2()
 
 camera_config = camera.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
-            camera.configure(camera_config)
-            camera.start()
-            time.sleep(2)
-            camera.capture_file("sample.jpg")
+camera.configure(camera_config)
+camera.start()
+time.sleep(2)
+camera.capture_file("sample.jpg")
 
 print("done")
-print("test")
