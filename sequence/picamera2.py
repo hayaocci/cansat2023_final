84dfa63a8b3eaf9409af009df55a5fe859ddcac8
hayaocci <7319079@ed.tus.ac.jp>
2023-06-19 19:03:10 +0900

    Update picamera2.py

--- sequence/picamera2.py
+++ sequence/picamera2.py
@@ -3,7 +3,7 @@ import time
 import traceback
 import os
 
-camera = Picamera2()
+camera = picamera.Picamera2()
 
 camera_config = camera.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
 camera.configure(camera_config)
