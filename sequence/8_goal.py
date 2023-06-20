948e6301ac88cdc41e76d57079261b7451e072fd
Rion-Suzuki <r.suzuki@kimura-lab.net>
2023-06-20 17:42:53 +0900

    変更

--- sequence/8_goal.py
+++ sequence/8_goal.py
@@ -1,10 +1,10 @@
 import sys
-sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/Motor')
+sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/motor')
 sys.path.append('/home/pi/git/kimuralab/Detection/GoalDetection')
 import time
 import traceback
 import goal_detection
-import Motor
+import motor
 
 goalArea = 0		#variable for goal area
 goalGAP = -1		#variable for goal gap
@@ -22,8 +22,8 @@ if __name__ == "__main__":
 		while 1:
 			if f == 0:
 				#-----------------get information-----------------#
-				Motor.motor(15, 15, 1.0)
-				Motor.motor(0, 0, 1.0)
+				motor.motor(15, 15, 1.0)
+				motor.motor(0, 0, 1.0)
 				goalFlug, goalArea, goalGAP, photoName = goal_detection.GoalDetection(photopath, H_min, H_max, S_thd, goalthd)
 				print("flug", goalFlug, "area", goalArea, "GAP", goalGAP, "photoname", photoName)
 				f = 1
@@ -37,16 +37,16 @@ if __name__ == "__main__":
 					f = 3
 				if f == 3:
 					T = float(input("input Time value "))
-					Motor.motor(L, R, T)
-					Motor.motor(0, 0, 2)
-					Motor.motor_stop()
+					motor.motor(L, R, T)
+					motor.motor(0, 0, 2)
+					motor.motor_stop()
 					f = 0
 			except KeyboardInterrupt:
 				print("Emergency!")
-				Motor.motor_stop()
+				motor.motor_stop()
 				sys.exit()
 			except:
 				pass
 	except:
-		Motor.motor_stop()
+		motor.motor_stop()
 		print(traceback.format_exc())
