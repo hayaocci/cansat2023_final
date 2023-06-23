cbf0817256fab5b7633ea0959b6f3190bb7261cf
hayaocci <7319079@ed.tus.ac.jp>
2023-06-23 20:12:33 +0900

    human distance

--- sequence/6_0_human_detection.py
+++ sequence/6_0_human_detection.py
@@ -1,1 +1,22 @@
+import gps_navigate
+import time
+import traceback
+
+#人検出の範囲内にいるかいないかの判定
+
+def distance_human(lat1, lon1, lat2, lon2):
+    data_dist_human = gps_navigate.vincenty_inverse(lat1, lon1, lat2, lon2)
+    try:
+        data_dist_human['distance'] <= 20
+        print("人検出の範囲内にいます")
+
+
+    except:
+        print("人検出の範囲外にいます")
+
+        print_im920sl(str(data_dist_human['distance']) + '----!!!    human   !!!')
+        return False
+
+
+
 
