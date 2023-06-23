c4edcffafad2b4f4a11fdc0e609a7cad3a77f3d4
hayaocci <7319079@ed.tus.ac.jp>
2023-06-23 20:43:53 +0900

    人検知の処理を追加

--- sequence/6_0_human_detection.py
+++ sequence/6_0_human_detection.py
@@ -1,25 +1,35 @@
-import gps_navigate
-import time
-import traceback
+import tflite_runtime.interpreter as tflite
+import numpy as np
+from PIL import Image
+import cv2
 import take
-import machine_learning
+import time
+import gps_navigate
 
 
+
+if __name__ == "__main__":
 #人検出の範囲内にいるかいないかの判定
-
-def distance_human(lat1, lon1, lat2, lon2):
-    data_dist_human = gps_navigate.vincenty_inverse(lat1, lon1, lat2, lon2)
-    
-    if data_dist_human['distance'] <= 20:
-        print("人検出の範囲内にいます")
+    def distance_human(lat1, lon1, lat2, lon2):
+        data_dist_human = gps_navigate.vincenty_inverse(lat1, lon1, lat2, lon2)
         
+        if data_dist_human['distance'] <= 20:
+            print("人検出の範囲内にいます")
+            
+            #人検知の処理
+            ML_people = DetectPeople(model_path="model_mobile.tflite" )
+            # image_path = 'imgs/hiroyuki.jpg'
+            # image_path = 'imgs/saru.jpg'
+            # ML_people.predict(image_path)
+            while 1:
+            img_path = take.picture('ML_imgs/image', 320, 240)
+            ML_people.predict(image_path=img_path)
+            # time.sleep(3)
 
-
-    else:
-        print("人検出の範囲外にいます")
-        
-       
-
+        else:
+            print("人検出の範囲外にいます")
+            #人検出の範囲外にいる場合は、人検出の範囲内に入るまで、ループする
+            return 
 
 
 
