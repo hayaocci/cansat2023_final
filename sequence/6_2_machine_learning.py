31603bfda640933b4adcbb1a50e6257325cd5cfc
Arashi1023 <nagasaki@kimura-lab.net>
2023-06-23 20:24:24 +0900

    回転6回設定

--- sequence/6_2_machine_learning.py
+++ sequence/6_2_machine_learning.py
@@ -58,13 +58,3 @@ if __name__ == "__main__":
         img_path = take.picture('ML_imgs/image', 320, 240)
         ML_people.predict(image_path=img_path)
         # time.sleep(3)
-
-
-if __name__ =="__main__":
-    start_time = time.time()
-    threshold = 20 * 60
-    elapsed_time = time.time()-start_time
-    if elapsed_time >= threshold:
-        print("A")#終了へ行くように変更して
-    else:
-        print("B")#6回繰り返すところへ
