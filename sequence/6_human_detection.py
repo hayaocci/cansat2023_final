5a4ef7f6b332a48b522633de8c7ab16dd6fe99e7
Arashi1023 <nagasaki@kimura-lab.net>
2023-06-23 19:58:52 +0900

    asas

--- sequence/6_human_detection.py
+++ sequence/6_human_detection.py
@@ -1,4 +1,15 @@
 import gps_navigate
 import time 
 
-if 
+
+
+if __name__ =="__main__":
+    start_time = time.time()
+    threshold = 20 * 60
+    elapsed_time = time.time()-start_time
+    if elapsed_time >= threshold:
+        print("A")#終了へ行くように変更して
+    else:
+        print("B")#6回繰り返すところへ
+        #
+        
