fc1cf65da667f86ce16b11f2718216bdb2faadbf
Arashi1023 <nagasaki@kimura-lab.net>
2023-06-23 20:34:10 +0900

    lon,latの順番入れ替えた

--- sequence/6_human_detection.py
+++ sequence/6_human_detection.py
@@ -4,9 +4,7 @@ import rotation
 import machine_learning #後で名前直して
 from math import sqrt
 
-def calculate_square_corners(lat1, lon1):#青点の設定
-     #lat1,lon1はゴールの座標
-    
+def calculate_square_corners(lon1, lat1):
     # 1度あたりの緯度経度の差（おおよその値）
     lat_diff_per_meter = 0.000009
     lon_diff_per_meter = 0.000011
@@ -15,19 +13,20 @@ def calculate_square_corners(lat1, lon1):#青点の設定
     square_side_length = 20
 
     # 正方形の角の座標を計算
-    lat2 = lat1 + (square_side_length / 2) * lat_diff_per_meter
-    lon2 = lon1 - (square_side_length / 2) * lon_diff_per_meter
+    lon2 = lon1 + (square_side_length / 2) * lon_diff_per_meter
+    lat2 = lat1 - (square_side_length / 2) * lat_diff_per_meter
 
-    lat3 = lat1 + (square_side_length / 2) * lat_diff_per_meter
     lon3 = lon1 + (square_side_length / 2) * lon_diff_per_meter
+    lat3 = lat1 + (square_side_length / 2) * lat_diff_per_meter
 
-    lat4 = lat1 - (square_side_length / 2) * lat_diff_per_meter
     lon4 = lon1 - (square_side_length / 2) * lon_diff_per_meter
+    lat4 = lat1 - (square_side_length / 2) * lat_diff_per_meter
 
-    lat5 = lat1 - (square_side_length / 2) * lat_diff_per_meter
-    lon5 = lon1 + (square_side_length / 2) * lon_diff_per_meter
+    lon5 = lon1 - (square_side_length / 2) * lon_diff_per_meter
+    lat5 = lat1 + (square_side_length / 2) * lat_diff_per_meter
 
     return lat2, lon2, lat3, lon3, lat4, lon4, lat5, lon5
+
 
 
 if __name__ =="__main__":
@@ -45,4 +44,5 @@ if __name__ =="__main__":
         for i in range(6):
             rotation()
         
-        print("C")#青点に移動するように変更して
+        #print("C")#青点に移動するように変更して
+        gps_running1.drive()
