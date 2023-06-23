606d936f44f73a3fde7a8b06d28369bd082a7f47
Arashi1023 <nagasaki@kimura-lab.net>
2023-06-23 22:21:32 +0900

    一応大枠　ループさせたいところ多め

--- sequence/6_human_detection.py
+++ sequence/6_human_detection.py
@@ -7,6 +7,7 @@ import take
 from math import sqrt
 
 #chatGPTさんより青点の設定
+#lon1,lat1は赤点の位置
 def calculate_square_corners(lon1, lat1):
     # 1度あたりの緯度経度の差（おおよその値）
     lat_diff_per_meter = 0.000009
@@ -37,32 +38,38 @@ if __name__ =="__main__":
     start_time = time.time()
     threshold = 20 * 60
     elapsed_time = time.time()-start_time
-    
+
     lat1 = 35.12345
     lon1 = 139.67890
 
     lat2_b, lon2_b, lat3_b, lon3_b, lat4_b, lon4_b, lat5_b, lon5_b = calculate_square_corners(lon1, lat1)
 
-    
 
-    if elapsed_time >= threshold:
-        print("A")#終了へ行くように変更して
-    else:
-        #6回繰り返すところへ
-        for i in range(6):
-            take()
-            DetectPeople()#人検知を行いたい、要変更
-            rotation()
+    #6回繰り返すところへ
+    for i in range(6):
         result=machine_learning.pro_people()
         #hitoの確率50%かどうか
         if result >=0.50:
-            print("A")#対象に近づく
+            print("A")#対象に近づいて終了へ
         else:
-            print("B")#もう一回遊べるドン
-        #青点に移動する
-        gps_running1.drive(lon2_b, lat2_b, thd_distance=10, t_adj_gps=60)
+            if elapsed_time >= threshold:#20分経ったか
+             print("A")#終了へ行くように変更して
+            else:
+                print("捜索続けます")
+        rotation()#プログラム要変更
 
-        #青点からxm以内であるか
-        if :
+    #青点に移動する
+    gps_running1.drive(lon2_b, lat2_b, thd_distance=10, t_adj_gps=60)
+    
+    def distance_bluearea1(lat2_b,lon2_b):#ここちょっと分からん
+        data_dist_bulearea1 =gps_navigate.vincenty_inverse(lat_now,lon_now,lat2_b,lon2_b)
+        #青点から5m以内か
+        if data_dist_bulearea1['distance']<=5:
+            print("第2捜索地点到達")
+            #「6回繰り返すところへ」に移動したい
         else:
+            print("B")#「青点に移動する」にループしたい
+
+    #↑これをbulearea1~4まで繰り返したい
+    
 
