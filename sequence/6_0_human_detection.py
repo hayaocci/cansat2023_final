f464625eb01d764f8b97220ab23c73f11fde172c
hayaocci <7319079@ed.tus.ac.jp>
2023-06-25 13:54:28 +0900

    古いコードを削除

--- sequence/6_0_human_detection.py
+++ sequence/6_0_human_detection.py
@@ -38,31 +38,7 @@ if __name__ == "__main__":
     else:
         print("人検知の範囲外にいます")
         print("人検知の範囲内に移動してください")
-        #人検知の範囲外にいる場合は、移動するように指示をする
-
-    '''
-    def distance_human(lat1, lon1, lat2, lon2):
-        data_dist_human = gps_navigate.vincenty_inverse(lat1, lon1, lat2, lon2)
-        
-        
-        if data_dist_human['distance'] <= 20:
-            print("人検出の範囲内にいます")
-            
-            #人検知の処理(machine_learning.pyからコピペした)
-            ML_people = DetectPeople(model_path="model_mobile.tflite" )
-            # image_path = 'imgs/hiroyuki.jpg'
-            # image_path = 'imgs/saru.jpg'
-            # ML_people.predict(image_path)
-            while 1:
-                img_path = take.picture('ML_imgs/image', 320, 240)
-                ML_people.predict(image_path=img_path)
-                # time.sleep(3)
-            
-
-        else:
-            print("人検出の範囲外にいます")
-            #人検出の範囲外にいる場合は、人検出の範囲内に入るまで、ループする
-        '''
+        #人検知の範囲外にいる場合は、移動するように指示をする。
 
 
 
