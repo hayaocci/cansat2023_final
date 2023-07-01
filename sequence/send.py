38fc7ecd67b2d07f9d9e3d9a14ab46d269465ba2
Arashi1023 <nagasaki@kimura-lab.net>
2023-07-01 14:26:45 +0900

    無線で使うやつ

---
+++ sequence/send.py
@@ -0,0 +1,11 @@
+import serial
+import time
+
+def send_data(data, port='/dev/ttyS0', baudrate=19200):
+    IM920Serial = serial.Serial(port, baudrate)
+    IM920Serial.flushOutput()
+    IM920Serial.write((data + '\r\n').encode())
+    IM920Serial.close()
+
+if __name__ == '__main__':
+    send_data("TXDU 0001.ABCD")
