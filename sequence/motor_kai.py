84be68dfab877ac533ca186b42d7c8d541550e90
hayaocci <7319079@ed.tus.ac.jp>
2023-07-04 19:10:07 +0900

    motor_kai作成

--- sequence/motor_kai.py
+++ sequence/motor_kai.py
@@ -1,1 +1,30 @@
 #motor.pyがちょっとわかりにくい気がするから、こっちを使おう。
+
+import time
+from gpiozero import Motor
+
+
+
+def setup():
+    """
+    モータのセットアップ
+    """
+    global motor_r, motor_l
+    Rpin1, Rpin2 = 5, 6
+    Lpin1, Lpin2 = 23, 18
+    motor_r = Motor(Rpin1, Rpin2)
+    motor_l = Motor(Lpin1, Lpin2)
+
+def motor_move(strength_l, strength_r, t_moving):
+    '''
+    引数の定義
+    strength_l == 左モータの強さ
+    strength_r == 右モータの強さ
+    t_moving == 動かす時間
+    '''
+
+    #モータのセットアップ
+    setup()
+    
+    strength_l = 
+    strength_r = 
