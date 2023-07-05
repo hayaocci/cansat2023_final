62751fefe9469db2ccbeae8078fc18440c2c6237
Arashi1023 <nagasaki@kimura-lab.net>
2023-07-05 21:06:34 +0900

    S→AMA

--- sequence/send.py
+++ sequence/send.py
@@ -1,14 +1,14 @@
 import serial
 import time
 
-def send_data(data, port='/dev/ttyS0', baudrate=19200):
+def send_data(data, port='/dev/ttyAMA0', baudrate=19200):
     IM920Serial = serial.Serial(port, baudrate)
     IM920Serial.flushOutput()
     IM920Serial.write((data + '\r\n').encode())
     IM920Serial.close()
 
 if __name__ == '__main__':
-    IM920Serial = serial.Serial('/dev/ttyS0', baudrate=19200)
+    IM920Serial = serial.Serial('/dev/ttyAMA0', baudrate=19200)
     IM920Serial.flushOutput()
     IM920Serial.write(("TXDU 0001,ABCD" + '\r\n').encode())
     IM920Serial.close()
