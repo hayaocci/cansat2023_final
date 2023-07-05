ad47eb7c045c7c16157a4ee2c872a0f5560e3dd7
Arashi1023 <nagasaki@kimura-lab.net>
2023-07-05 13:10:50 +0900

    2021のコードに一部変更

--- sequence/gps_running1.py
+++ sequence/gps_running1.py
@@ -219,10 +219,9 @@ def drive(lon2, lat2, thd_distance, t_adj_gps, logpath='/home/dendenmushi/cansat
                         else:
                             adj = -30
                     print(f'angle ----- {theta}')
-                    strength_l, strength_r = adj,- adj
+                    strength_l, strength_r = 70 + adj ,70 - adj - adj_r
                     #motor.motor_continue(strength_l, strength_r)
-                    motor.move(strength_l, strength_r, 0.2)
-                    motor.move(50,50,2)
+                    motor.motor_continue(strength_l, strength_r)
                     #time.sleep(0.04)
                     time.sleep(0.4)
             t_stuck_count += 1
