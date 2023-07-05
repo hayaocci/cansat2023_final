ed0244a0ef785725a5b3f4de305c0b0a1b35a39f
Arashi1023 <nagasaki@kimura-lab.net>
2023-07-05 20:57:53 +0900

    緒と変更

--- sequence/send.py
+++ sequence/send.py
@@ -8,5 +8,8 @@ def send_data(data, port='/dev/ttyS0', baudrate=19200):
     IM920Serial.close()
 
 if __name__ == '__main__':
-    send_data("TXDU 0001,ABCD")
+    IM920Serial = serial.Serial('/dev/ttyS0', baudrate=19200)
+    IM920Serial.flushOutput()
+    IM920Serial.write(("TXDU 0001,ABCD" + '\r\n').encode())
+    IM920Serial.close()
     print('送信しました')
