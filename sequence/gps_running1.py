5ea2fa88cf545e3dcf04d6e725b439f342080024
hayaocci <7319079@ed.tus.ac.jp>
2023-07-05 14:04:30 +0900

    line132 calibrationのモータ変更

--- sequence/gps_running1.py
+++ sequence/gps_running1.py
@@ -129,7 +129,7 @@ def drive(lon2, lat2, thd_distance, t_adj_gps, logpath='/home/dendenmushi/cansat
         # xbee.str_trans('calibration Start')
         other.print_im920sl('##--calibration Start--##\n')
         print("------calibration Start------")
-        magx_off, magy_off = calibration.cal(40, 40, 30)
+        magx_off, magy_off = calibration.cal(40, -40, 30)
         print(f'magx_off: {magx_off}\tmagy_off: {magy_off}\n')
         print("------calibration finished------")
 
