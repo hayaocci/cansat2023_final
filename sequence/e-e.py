4840d958fa55a08463ab328cc39b9117d81abbee
Arashi1023 <nagasaki@kimura-lab.net>
2023-07-06 14:32:05 +0900

    名前変更

--- sequence/e-e.py
+++ sequence/e-e.py
@@ -1,9 +1,9 @@
 import release
 import land
-import 3_melt#名前変更して
-import 4_parachute_avoid#名前変更して
+import melt#名前変更して
+import parachute_avoid#名前変更して
 import gps_running1
-import 6_human_detection#名前変更して
+import human_detection#名前変更して
 import gps_running2
 import photo_running
 import send
