3144aa54e517100f09f00a28419ddf300ab32c04
Arashi1023 <nagasaki@kimura-lab.net>
2023-07-06 11:47:13 +0900

    set

--- sequence/send.py
+++ sequence/send.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     text = str(input())
     IM920Serial = serial.Serial('/dev/ttyAMA0', baudrate=19200)
     IM920Serial.flushOutput()
-    IM920Serial.write(("TXDU 0001," + text + '\r\n').encode())
+    #IM920Serial.write(("TXDU 0001," + text + '\r\n').encode())
+    IM920Serial.write(( text + '\r\n').encode())
     IM920Serial.close()
     print('送信しました')
