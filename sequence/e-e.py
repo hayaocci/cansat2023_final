13d00cde9926c54fc0c3135a07ab8a937b50acb6
Arashi1023 <nagasaki@kimura-lab.net>
2023-07-06 14:44:03 +0900

    後でsetupはまとめよう

--- sequence/e-e.py
+++ sequence/e-e.py
@@ -1,20 +1,29 @@
 import release
 import land
-import melt#名前変更して
-import parachute_avoid#名前変更して
+import melt
+import parachute_avoid
 import gps_running1
-import human_detection#名前変更して
-import gps_running2
+import human_detection
 import photo_running
-import send
+
+import bmx055
 import bme280
+import send
+import motor
+import traceback
+import pigpio
+import time
+import gps
+import take
+import paradetection
+from machine_learning import DetectPeople
+import sys
+import calibration
+
 
 if __name__=='__main__':
-######--------------setup--------------######
-
-######--------------release--------------######
-    release()
-    '''''
+###-------release judge -------###
+    print("START: Release judge")
     thd_press_release = 0.1
     pressreleasecount = 0
     pressreleasejudge = 0
@@ -24,55 +33,58 @@ if __name__=='__main__':
     press_d = 0
 
     while True:
-        press_count_release, press_judge_release = pressdetect_release(thd_press_release, t_delta_release)
+        press_count_release, press_judge_release = release.pressdetect_release(thd_press_release, t_delta_release)
         print(f'count:{pressreleasecount}\tjudge{pressreleasejudge}')
-        if press_count_release  >= 3:
+        if press_count_release  > 3:
             print('Press')
-            send.send_data("TXDU 0001.0001")
+            send.send_data("TXDU 0001.A001")
             break
         else:
             print('unfulfilled')
-    send.send_data("TXDU 0001.0002")
 
-    '''''
-######--------------land--------------######
-    land()
-    '''''
-    print("Start")
+    send.send_data("TXDU 0001.AAAA")
 
-    bme280.bme280_setup()
-    bme280.bme280_calib_param()
+    ###-------land judge -------###
+    print("START: Land judge")
+    send.send_data("TXDU 0001,B000")
+
+    #bme280.bme280_setup()
+    #bme280.bme280_calib_param()
 
     landcount = 0
     pressdata = [0.0, 0.0, 0.0, 0.0]
 
     while True:
         presslandjudge = 0
-        landcount, presslandjudge = pressdetect_land(0.1)
+        landcount, presslandjudge = land.pressdetect_land(0.1)
         print(f'count:{landcount}\tjudge:{presslandjudge}')
         if presslandjudge == 1:
             print('Press')
+            send.send_data("TXDU 0001,B002")
             print('##--landed--##')
             break
         else:
             print('Press unfulfilled')
-    '''''
+            send.send_data("TXDU 0001,B001")
 
-######--------------melt--------------######
-    3_melt()
-    '''''
+    send.send_data("TXDU 0001,BBBB")
+    ###-------melt-------###
+
+    print("START: Melt")
+    pi = pigpio.pi()
+
+    meltPin = 4
     try:
-		down()
-	except:
-		pi.write(meltPin, 0)
-    '''''
-
-######--------------para_avoid--------------######
-    4_parachute_avoid()
-    '''''
+        melt.down()
+        send.send_data("TXDU 0001,C001")
+    except:
+        pi.write(meltPin, 0)
+    
+    send.send_data("TXDU 0001,CCCC")
+    ###------paraavo-------###
     try:
         motor.setup()
-        
+
         print("START: Parachute avoidance")
 
         flug, area, gap, photoname = paradetection.para_detection("photostorage/photostorage_paradete/para", 320, 240,
@@ -84,7 +96,7 @@ if __name__=='__main__':
             flug, area, gap, photoname = paradetection.para_detection("photostorage/photostorage_paradete/para", 320,
                                                                       240, 200, 10, 120, 1)
             print(f'flug:{flug}\tarea:{area}\tgap:{gap}\tphotoname:{photoname}')
-            parachute_avoidance(flug, gap)
+            parachute_avoid.parachute_avoidance(flug, gap)
             print(flug)
             if flug == -1 or flug == 0:
                 count_paraavo += 1
@@ -98,23 +110,40 @@ if __name__=='__main__':
     except:
         print(traceback.format_exc())
     print("finish!")
-    '''''
+
+    send.send_data("TXDU 0001,DDDD")
+
 
 ######--------------run1--------------######
-gps_running1()
-'''''
-    lat2 = 35.923914
-    lon2 = 139.912223
+    #生協入口
+    #lat2 = 35.91818718
+    #lon2 = 139.90814829
 
+    #12号館前
+    #lat2 = 35.91896917
+    #lon2 = 139.90859362
+
+    #グランドのゴール前
+    #lat2 = 35.923914
+    #lon2 = 139.912223
+
+    #狭いグランドのほう
+    #lat2 = 35.9243874
+    #lon2 = 139.9114187
+
+    #中庭の芝生
+    lat2 = 35.91817415
+    lon2 = 139.90825559
+
+    #実験棟の前
+    #lat2 = 35.9189778
+    #lon2 = 139.9071493 
     gps.open_gps()
     bmx055.bmx055_setup()
     motor.setup()
 
-    drive(lon2, lat2, thd_distance=10, t_adj_gps=60)
-'''''
+    gps_running1.drive(lon2, lat2, thd_distance=10, t_adj_gps=100)
 ######--------------mission--------------######
-6_human_detection()
-'''''
     count = 0
     human_judge_count=0
     break_outer_loop =False
@@ -129,12 +158,19 @@ gps_running1()
     #lon_human = 139.90859362
 
     #グランドのゴール前
-    lat_human = 35.923914
-    lon_human = 139.912223
+    #lat_human = 35.923914
+    #lon_human = 139.912223
+
+    #lat_human = 35.9243467
+    #lon_human = 139.9113996
+
+    #中庭の芝生
+    lat_human = 35.91817415
+    lon_human = 139.90825559
 
     ML_people = DetectPeople(model_path="model_mobile.tflite" )
 
-    lat_n, lon_n, lat_e, lon_e, lat_s, lon_s, lat_w, lon_w = get_locations(lat_human, lon_human)
+    lat_n, lon_n, lat_e, lon_e, lat_s, lon_s, lat_w, lon_w = human_detection.get_locations(lat_human, lon_human)
 
     #まずはメインエリアを捜索
     for k in range(6):
@@ -160,6 +196,7 @@ gps_running1()
                         print("遭難者発見")
                         break
             if break_outer_loop:
+                human_judge_count = 0
                 break
         else:
             if elapsed_time >= threshold:  # 20分経ったか
@@ -167,7 +204,7 @@ gps_running1()
                 break
             else:
                 print("捜索続けます")
-        motor.motor_move(1, -1, 0.5)  # 調整必要
+        motor.move(50, -50, 0.5)  # 調整必要
 
     if human_judge_count==0:
         print ("青点エリア捜索に移行")
@@ -176,15 +213,12 @@ gps_running1()
             if break_outer_loop:
                 break
             lat_now, lon_now = gps.location()
-            move_to_bulearea()
-            take_and_rotation()
-'''''
+            human_detection.move_to_bulearea(count)
+            human_detection.take_and_rotation(break_outer_loop, human_judge_count)
 ######--------------run2--------------######
-gps_running2()
+    gps_running1.drive(lon2, lat2, thd_distance=10, t_adj_gps=100)
 #lon,latの値変えるだけでよさそう
 ######--------------goal--------------######
-photo_running()
-'''''
     try:
         # Initialize
         #lat2 = 35.9192621
@@ -197,8 +231,15 @@ photo_running()
         #lon2 = 139.90859362
 
         #グランドのゴール前
-        lat2 = 35.923914
-        lon2 = 139.912223
+        #lat2 = 35.923914
+        #lon2 = 139.912223
+
+        #lat2 = 35.9243426
+        #lon2 = 139.9112739
+
+        #中庭の芝生
+        lat2 = 35.91817415
+        lon2 = 139.90825559
 
         G_thd = 60
         log_photorunning = '/home/dendenmushi/cansat2023/log/photorunning_practice.txt'
@@ -211,8 +252,8 @@ photo_running()
         #print_im920sl('##--calibration end--##')
 
         # Image Guide
-        image_guided_driving(log_photorunning, G_thd, magx_off,
-                             magy_off, lon2, lat2, thd_distance=5, t_adj_gps=60)
+        photo_running.image_guided_driving(log_photorunning, G_thd, magx_off,
+                             magy_off, lon2, lat2, thd_distance=5, t_adj_gps=10)
 
     except KeyboardInterrupt:
         #print_im920sl('stop')
@@ -221,6 +262,3 @@ photo_running()
     except Exception as e:
         #im920sl2.off()
         tb = sys.exc_info()[2]
-        #print_im920sl("message:{0}".format(e.with_traceback(tb)))
-
-'''''
