7c0e89f9ea02f73cf280f907951417fba26b1d94
ryuki <109502009+kokom24@users.noreply.github.com>
2023-07-10 16:04:11 +0900

    Update IM920sl_test2.py

--- sequence/IM920sl_test2.py
+++ sequence/IM920sl_test2.py
@@ -1,24 +1,35 @@
 import serial
-import time
 
-def send_data(data, port='/dev/ttyAMA0', baudrate=19200):
-    IM920Serial = serial.Serial(port, baudrate)
-    IM920Serial.flushOutput()
-    IM920Serial.write((data + '\r\n').encode())
-    IM920Serial.close()
+def send_data(serial_port, data):
+    serial_port.write((data + '\r\n').encode())
 
-def receive_data(port='/dev/ttyAMA0', baudrate=19200):
-    IM920Serial = serial.Serial(port, baudrate)
-    IM920Serial.flushInput()
-    received_data = IM920Serial.readline().strip()
-    IM920Serial.close()
+def receive_data(serial_port):
+    received_data = serial_port.readline().strip().decode()
     return received_data
 
 if __name__ == '__main__':
-    print("無線の初期設定")
-    text = input("送信するデータを入力してください: ")
-    send_data(text)
-    time.sleep(1)
-    received_data = receive_data()
-    print("受信データ:", received_data)
+    port = '/dev/ttyAMA0'
+    baudrate = 19200
 
+    # シリアルポートの初期化
+    serial_port = serial.Serial(port, baudrate)
+    serial_port.timeout = 1
+
+    while True:
+        # 送信データの入力
+        send_text = input("送信するデータを入力してください ('q'で終了): ")
+        if send_text == 'q':
+            break
+
+        # データの送信
+        send_data(serial_port, send_text)
+
+        # データの受信
+        received_text = receive_data(serial_port)
+
+        # 結果の表示
+        print("送信データ:", send_text)
+        print("受信データ:", received_text)
+
+    # シリアルポートのクローズ
+    serial_port.close()
