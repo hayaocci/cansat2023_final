4828f4501d9b3e96c0f32b39b975c86221cd803a
hayaocci <7319079@ed.tus.ac.jp>
2023-07-11 21:16:19 +0900

    a

--- sequence/beta_save_photo.py
+++ sequence/beta_save_photo.py
@@ -1,6 +1,13 @@
 #上書き保存されないようにするために、ファイル名を日付にする
-
 import datetime
+import cv2
 
 dt_now = datetime.datetime.now()
 print(dt_now)
+
+def save_img(save_img_path, img_name, save_img):
+    dt_now = datetime.datetime.now()
+    final_name = save_img_path + img_name + str(dt_now) + ".jpg"
+    cv2.imwrite(final_name, save_img)
+
+if __name__ == '__main__':
