78e92d9cc6253bff6fc42480180ef8ba6cabc85a
hayaocci <7319079@ed.tus.ac.jp>
2023-07-11 16:40:29 +0900

    繰り返し処理を実装

--- sequence/beta_photo_running.py
+++ sequence/beta_photo_running.py
@@ -121,50 +121,67 @@ def image_guided_driving(area_ratio, angle_beta):
     area_ratio, angle_beta = detect_goal()
 
     try:
-        while area_ratio == 0:
-            print("ゴールが見つかりません。回転します。")
-            motor.move(40, -40, 0.1)
-            area_ratio, angle_beta = detect_goal()
-        print("ゴールを捉えました。ゴールへ向かいます。")
-        
-        area_ratio, angle_beta = detect_goal()
+        while 1:
+            if area_ratio == 100:
+                break
 
-        while 0 < area_ratio < 80:
-            #cansatの真正面にゴールがないとき
-            while angle_beta != 3:
-                if angle_beta == 1:
-                    motor.move(-20, 20, 0.5)
-                elif angle_beta == 2:
-                    motor.move(-20, 20, 0.3)
-                elif angle_beta == 4:
-                    motor.move(20, -20, 0.3)
-                elif angle_beta == 5:
-                    motor.move(20, -20, 0.5)
-                
+            while area_ratio == 0:
+                print("ゴールが見つかりません。回転します。")
+                motor.move(40, -40, 0.1)
+                area_ratio, angle_beta = detect_goal()
+            else:
+                if area_ratio == 100:
+                    break
+                print("ゴールを捉えました。ゴールへ向かいます。")
                 area_ratio, angle_beta = detect_goal()
 
-            print("正面にゴールがあります。直進します。")
+                while 0 < area_ratio < 100:
+                    if area_ratio == 100:
+                        break
 
-            #cansatの真正面にゴールがあるとき
-            if 60 < area_ratio <= 80:
-                t_running = 0.5
-            elif 40 < area_ratio <= 60:
-                t_running = 0.3
-            elif 0 < area_ratio <= 40:
-                t_running = 0.1
+                    #cansatの真正面にゴールがないとき
+                    while angle_beta != 3:
+                        if angle_beta == 1:
+                            motor.move(-20, 20, 0.5)
+                        elif angle_beta == 2:
+                            motor.move(-20, 20, 0.3)
+                        elif angle_beta == 4:
+                            motor.move(20, -20, 0.3)
+                        elif angle_beta == 5:
+                            motor.move(20, -20, 0.5)
+                        
+                        area_ratio, angle_beta = detect_goal()
+
+                    print("正面にゴールがあります。直進します。")
+
+                    #cansatの真正面にゴールがあるとき
+                    if area_ratio == 100:
+                        print("目的地周辺に到着しました。案内を終了します。")
+                        print("お疲れさまでした。")
+                        break
+                    
+                    elif 60 < area_ratio <= 80:
+                        t_running = 0.1
+                    elif 40 < area_ratio <= 60:
+                        t_running = 0.2
+                    elif 0 < area_ratio <= 40:
+                        t_running = 0.4
+                    
+                    motor.move(30, 30, t_running)
+                    area_ratio, angle_beta = detect_goal()
+
+                else:
+                    print("ゴールを見失いました。ゴールを捉えるまで回転します。")
+
             
-            motor.move(30, 30, t_running)
+        
 
-            area_ratio, angle_beta = detect_goal()
-
-        print("目的地周辺に到着しました。案内を終了します。")
-        print("お疲れさまでした。")
+        print("ゴールしてループから抜け出せました。停止します。")
 
     except KeyboardInterrupt:
         print("stop")
     # except Exception as e:
     #     tb = sys.exc_info()[2]
-
 
 if __name__ == "__main__":
 
