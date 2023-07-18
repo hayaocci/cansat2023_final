8a5ec56648661057bc59de01a191de47f9172d66
hayaocci <7319079@ed.tus.ac.jp>
2023-07-18 18:27:02 +0900

    a

---
+++ sequence/epower_test.py
@@ -0,0 +1,24 @@
+import motor
+import time
+
+def e_power_test(t_move):
+    print("-----Start e_power_test-----")
+    s_time = time.time()
+    while True:
+        motor.move(40, 40, t_move)
+        elapsed_time = time.time() - s_time
+        print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+        time.sleep(10)
+        
+        if elapsed_time > 1200:
+            break
+
+    print("10mins run finish")
+
+        
+
+
+if __name__ == '__main__':
+    motor.setup()
+    #2分間走らせて、10秒休憩
+    e_power_test(120)
