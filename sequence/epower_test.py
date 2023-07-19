d7a8d1db8c52014dfb9f87a6fb8b4739f197d978
hayaocci <7319079@ed.tus.ac.jp>
2023-07-19 10:33:55 +0900

    a

--- sequence/epower_test.py
+++ sequence/epower_test.py
@@ -6,13 +6,13 @@ def e_power_test(t_move):
     s_time = time.time()
     while True:
         motor.move(35, 35, t_move)
-        elapsed_time = time.time() - s_time
-        print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
         time.sleep(10)
 
         #calibration
         motor.move(35, -35, 1.6)
         time.sleep(3)
+        elapsed_time = time.time() - s_time
+        print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
         
 
 
