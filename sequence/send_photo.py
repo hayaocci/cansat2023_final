1e5f7b6840d1e9fad820ab8c988b29efe42d473e
ryuki <109502009+kokom24@users.noreply.github.com>
2023-07-22 18:55:07 +0900

    Update send_photo.py
    GPSと写真撮るようにした

--- sequence/send_photo.py
+++ sequence/send_photo.py
@@ -9,6 +9,38 @@ import pigpio
 import numpy as np
 import traceback
 import send
+import wireless_communication
+import cv2
+
+RX = 27
+pi = pigpio.pi()
+chunk_size = 8  # 1回に表示するバイト数
+delay = 0.2  # 表示間隔（秒）
+
+def capture_and_save_photo(file_name):
+    # カメラを起動
+    camera = cv2.VideoCapture(0)
+    
+    # カメラが正常にオープンされたか確認
+    if not camera.isOpened():
+        print("カメラをオープンできませんでした。")
+        return
+    
+    # 画像を撮影
+    ret, frame = camera.read()
+    
+    if not ret:
+        print("画像の取得に失敗しました。")
+        camera.release()
+        return
+    
+    # 画像をファイルに保存
+    cv2.imwrite(file_name, frame)
+    print("写真が保存されました。")
+    
+    # カメラを解放
+    camera.release()
+
 
 def compress_image(image_path, output_path, quality):
     image = Image.open(image_path)
@@ -22,12 +54,44 @@ def image_file_to_base64(file_path):
     return data.decode('utf-8')
 
 
+#---------------------GPS情報送信--------------------------
+#gps情報取得
+wireless_communication.open_gps()
+
+num_samples = 5
+lat_sum = 0.0
+lon_sum = 0.0
+
+for _ in range(num_samples):
+    lat, lon = wireless_communication.location()
+    lat_sum += lat
+    lon_sum += lon
+
+# 平均計算
+avg_lat = lat_sum / num_samples
+avg_lon = lon_sum / num_samples
+
+# 無線で送信
+send.send_data("human_GPS_start")
+time.sleep(delay)
+send.sed_data(avg_lat,avg_lon)
+time.sleep(delay)
+send.send_data("human_GPS_fin")
+time.sleep(delay)
+
+
+
+#---------------------画像伝送-----------------------------
+
+file_name = "/home/dendenmushi/cansat2023/sequence/ML_imgs/sendtest_photo.jpg"  # 保存するファイル名を指定
+capture_and_save_photo(file_name)
+
 # 圧縮したい画像のパスと出力先のパスを指定します
-input_image_path = '/home/dendenmushi/cansat2023/sequence/ML_imgs/send_photo_sample.jpg'
+input_image_path = '/home/dendenmushi/cansat2023/sequence/ML_imgs/sendtest_photo.jpg'
 compressed_image_path = 'compressed_test.jpg'
 
 # 圧縮率を指定します（0から100の範囲の整数）
-compression_quality = 80
+compression_quality = 50
 
 # 画像を圧縮します
 compress_image(input_image_path, compressed_image_path, compression_quality)
@@ -38,8 +102,6 @@ with open(compressed_image_path, 'rb') as f:
 
 
 data = compressed_image_binary  # バイナリデータを指定してください
-chunk_size = 8  # 1回に表示するバイト数
-delay = 0.1  # 表示間隔（秒）
 output_filename = "output.txt"  # 保存先のファイル名
 
 start_time = time.time()  # プログラム開始時刻を記録
@@ -67,7 +129,3 @@ execution_time = end_time - start_time  # 実行時間を計算
 
 print("実行時間:", execution_time, "秒")
 print("データを", output_filename, "に保存しました。")
-
-
-
-
