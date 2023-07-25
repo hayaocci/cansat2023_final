b631b52972068d9cbacbc01c952ab8eb6dc99e12
Arashi1023 <nagasaki@kimura-lab.net>
2023-07-25 17:46:44 +0900

    a

--- sequence/send_photo.py
+++ sequence/send_photo.py
@@ -397,17 +397,17 @@ if __name__ == '__main__':
     #---------------------画像伝送----------------------------#
     
     
-    photo_take = picture(file_name, 320, 240)
+    photo_naem = picture(file_name, 80, 60)
     print("撮影した写真のファイルパス：", photo_name)
     
     # 入力ファイルパスと出力ファイルパスを指定してリサイズ
-    input_file = photo_take    # 入力ファイルのパスを適切に指定してください
-    output_file = "/home/dendenmushi/cansat2023/sequence/ML_imgs/send_photo_resize.jpg"  # 出力ファイルのパスを適切に指定してください
-    new_width = 80            # リサイズ後の幅を指定します
-    new_height = 60           # リサイズ後の高さを指定します
+    #input_file = photo_take    # 入力ファイルのパスを適切に指定してください
+   #output_file = "/home/dendenmushi/cansat2023/sequence/ML_imgs/send_photo_resize.jpg"  # 出力ファイルのパスを適切に指定してください
+   # new_width = 80            # リサイズ後の幅を指定します
+   ## new_height = 60           # リサイズ後の高さを指定します
 
     # リサイズを実行
-    resize_image(input_file, output_file, new_width, new_height)
+    #resize_image(input_file, output_file, new_width, new_height)
 
 
     
