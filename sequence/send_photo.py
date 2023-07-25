21b33d0adbfcf0ccf1f6948b92a7ec6bccdbda24
Arashi1023 <nagasaki@kimura-lab.net>
2023-07-25 17:35:44 +0900

    a

--- sequence/send_photo.py
+++ sequence/send_photo.py
@@ -1,4 +1,5 @@
 import time
+import os
 from PIL import Image
 import base64
 from jinja2 import Environment, FileSystemLoader
@@ -326,6 +327,18 @@ def image_file_to_base64(file_path):
 
     return data.decode('utf-8')
 
+def resize_image(input_path, output_path, new_width, new_height):
+    try:
+        # 画像を開く
+        with Image.open(input_path) as img:
+            # リサイズ
+            resized_img = img.resize((new_width, new_height), Image.LANCZOS)
+            # 出力ファイルに保存
+            resized_img.save(output_path)
+            print(f"画像をリサイズして保存しました: {output_path}")
+    except Exception as e:
+        print(f"エラーが発生しました: {e}")
+
 
 #-----------------------------------------------------------------------------------------------------------#
 if __name__ == '__main__':
@@ -384,8 +397,18 @@ if __name__ == '__main__':
     #---------------------画像伝送----------------------------#
     
     
-    photo_name = picture(file_name, 320, 240)
+    photo_take = picture(file_name, 320, 240)
     print("撮影した写真のファイルパス：", photo_name)
+    
+    # 入力ファイルパスと出力ファイルパスを指定してリサイズ
+    input_file = photo_take    # 入力ファイルのパスを適切に指定してください
+    output_file = "/home/dendenmushi/cansat2023/sequence/ML_imgs/send_photo_resize.jpg"  # 出力ファイルのパスを適切に指定してください
+    new_width = 80            # リサイズ後の幅を指定します
+    new_height = 60           # リサイズ後の高さを指定します
+
+    # リサイズを実行
+    resize_image(input_file, output_file, new_width, new_height)
+
 
     
     print("写真撮影完了")
