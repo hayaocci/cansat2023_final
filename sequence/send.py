f1d783c4ba45af3117287cb15be1aa2d9dc3d430
Arashi1023 <nagasaki@kimura-lab.net>
2023-07-27 21:39:48 +0900

    test

--- sequence/send.py
+++ sequence/send.py
@@ -1,5 +1,10 @@
 import serial
 import time
+import pigpio
+
+pi = pigpio.pi()
+sendPin = 22
+count=0
 
 def send_data(data, port='/dev/ttyAMA0', baudrate=19200):
     IM920Serial = serial.Serial(port, baudrate)
@@ -7,8 +12,22 @@ def send_data(data, port='/dev/ttyAMA0', baudrate=19200):
     IM920Serial.write(("TXDU 0001,"+data + '\r\n').encode())
     IM920Serial.close()
 
+def send_reset(t_reset = 10):
+    """
+	無線をリセットするための関数
+	"""
+    pi.write(sendPin, 0)
+    time.sleep(1)
+    pi.write(sendPin, 1)
+    time.sleep(t_reset)
+    pi.write(sendPin, 0)
+    time.sleep(1)
+
 if __name__ == '__main__':
     while 1:
         text = str(input())
-        send_data(text)
-        print('送信しました')
+        if text =="A":
+            send_reset
+        else:
+            send_data(text)
+            print('送信しました')
