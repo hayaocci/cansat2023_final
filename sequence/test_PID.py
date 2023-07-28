ccb7cc2349e8114ed6b8cab367535960f8aa038f
hayaocci <7319079@ed.tus.ac.jp>
2023-07-28 21:03:18 +0900

    a

--- sequence/test_PID.py
+++ sequence/test_PID.py
@@ -168,7 +168,7 @@ def adjust_direction_north(magx_off, magy_off, theta_array: list):
         check = 0
         bool_com = True
         for i in range(len(theta_array)):
-            if abs(theta_array[i]) > 10:
+            if abs(theta_array[i]) > 15:
                 bool_com = False
                 break
         if bool_com:
