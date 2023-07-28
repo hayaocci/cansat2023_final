bf0d6c862fe0286e49c91ce82722796e0a23ed85
hayaocci <7319079@ed.tus.ac.jp>
2023-07-28 21:46:03 +0900

    a

--- sequence/test_PID.py
+++ sequence/test_PID.py
@@ -130,7 +130,7 @@ def adjust_direction_north(target_theta, magx_off, magy_off, theta_array: list):
             Kd = Kd_
         else:
             Ki = Ki_
-            Kd = 4
+            Kd = 5
 
         #-----角度の取得-----#
         magdata = bmx055.mag_dataRead()
