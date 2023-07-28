adbb66fa0ef303116d3d24c10760ac5c36f8caa0
hayaocci <7319079@ed.tus.ac.jp>
2023-07-28 20:58:03 +0900

    a

--- sequence/test_PID.py
+++ sequence/test_PID.py
@@ -95,7 +95,7 @@ def PID_control(theta, theta_array: list, Kp=0.5, Ki=0.5, Kd=0.5):
 def adjust_direction_north(magx_off, magy_off, theta_array: list):
     
     #パラメータの設定
-    Kp = 0.6
+    Kp = 0.4
     Kd = 1.1
     Ki = 0.01
     
