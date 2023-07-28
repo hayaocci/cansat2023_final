76865671a9eb5edb31e5b68472f80136af8562e8
hayaocci <7319079@ed.tus.ac.jp>
2023-07-28 21:27:36 +0900

    a

--- sequence/test_PID.py
+++ sequence/test_PID.py
@@ -96,7 +96,7 @@ def adjust_direction_north(target_theta, magx_off, magy_off, theta_array: list):
     
     #パラメータの設定
     Kp = 0.3
-    Kd = 1.5
+    Kd = 1.8
     Ki = 0.04
     
     print('adjust_direction_north')
