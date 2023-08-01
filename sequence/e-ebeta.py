6c67bd27e0bda7928a3f92e4e166a04be6904515
Arashi1023 <nagasaki@kimura-lab.net>
2023-08-01 20:31:04 +0900

    a

--- sequence/e-ebeta.py
+++ sequence/e-ebeta.py
@@ -31,6 +31,7 @@ import wgps_beta_photo_running as imgguide
 from math import sqrt
 #variable for log
 log_phase=other.filename('/home/dendenmushi/cansat2023/sequence/log/phaselog','txt')
+log_report=other.filename('/home/dendenmushi/cansat2023/sequence/log/reportlog','txt')
 log_release=other.filename('/home/dendenmushi/cansat2023/sequence/log/releaselog','txt')
 log_landing=other.filename('/home/dendenmushi/cansat2023/sequence/log/landinglog','txt')
 log_melting=other.filename('/home/dendenmushi/cansat2023/sequence/log/meltinglog','txt')
@@ -210,6 +211,7 @@ if __name__=='__main__':
     other.log(log_phase,'0',"phase","Time","Elapsed Time","lat","lon")
     other.log(log_phase,'1',"setup phase",datetime.datetime.now(),time.time()-t_start,lat_log,lon_log)
     phase=other.phase(log_phase)
+    other.phase(log_report,datetime.datetime.now()+"(PDT)",time.time()-t_start,"N"+lat_log,"W"+lon_log)
     bme280.bme280_setup()
     bme280.bme280_calib_param()
 
@@ -710,4 +712,5 @@ if __name__=='__main__':
     time.sleep(10)
     lat_log,lon_log=gps.location()
     other.log(log_phase,'10',"all phase complete",datetime.datetime.now(),time.time()-t_start,lat_log,lon_log)
+    other.phase(log_report,datetime.datetime.now()+"(PDT)",time.time()-t_start,"N"+lat_log,"W"+lon_log)
     print("all complete!")
--- sequence/report.py
+++ sequence/report.py
@@ -58,7 +58,23 @@ def extract_and_write(input_file, output_file, start_line, end_line=float('inf')
     except Exception as e:
         print(f"エラーが発生しました: {e}")
 
-def add_text_to_last_line(input_file, output_file, text_to_add):
+def extract_single_line(input_file, output_file, line_number):
+    try:
+        with open(input_file, 'r') as infile:
+            lines = infile.readlines()
+
+        # 指定した行を抽出
+        line_to_extract = lines[line_number - 1]
+
+        # ファイルの内容を追記モードで開き、元の内容を保持したまま新しい内容を追記
+        with open(output_file, 'a') as outfile:
+            outfile.write(line_to_extract)
+
+        print(f"{line_number}行目が {output_file} に書き込まれました。")
+    except Exception as e:
+        print(f"エラーが発生しました: {e}")
+
+def add_text_to_last_line(output_file, text_to_add):
     try:
         with open(output_file, 'a') as outfile:
             # 「あいうえお」を追加して新しいファイルに書き込む
@@ -68,23 +84,80 @@ def add_text_to_last_line(input_file, output_file, text_to_add):
     except Exception as e:
         print(f"エラーが発生しました: {e}")
 
-# 使用例：抽出する行の範囲を3行目からファイルの最後までに指定
-input_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/log/phaselog0109.txt'
+#report file
 output_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/control_record_report.txt'
-start_line = 3
-extract_and_write(input_file, output_file, start_line)
+
+input_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/log/reportlog.txt'
+line_number=1
+extract_single_line(input_file, output_file, line_number)
 print("1")
 
-# 使用例：元のファイルの内容を読み込み、最下行に文章を追加して新しいファイルとして保存する
 text_to_add = "Time and position at which the control ended:"
-add_text_to_last_line(input_file, output_file, text_to_add)
+add_text_to_last_line(output_file, text_to_add)
 print("2")
 
-input_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/log/phaselog0109.txt'
-start_line = 3
-extract_and_write(input_file, output_file, start_line)
+line_number=2
+extract_single_line(input_file, output_file, line_number)
 print("3")
 
 text_to_add = "All control history:"
-add_text_to_last_line(input_file, output_file, text_to_add)
+add_text_to_last_line(output_file, text_to_add)
 print("4")
+
+#log_phase
+input_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/log/phaselog.txt'
+start_line = 1
+extract_and_write(input_file, output_file, start_line)
+print("5")
+
+text_to_add = "Detailed control record"
+add_text_to_last_line(input_file, output_file, text_to_add)
+print("6")
+
+#log_release
+input_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/log/releaselog.txt'
+start_line = 1
+extract_and_write(input_file, output_file, start_line)
+print("7")
+
+#log_landing
+input_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/log/landinglog.txt'
+start_line = 1
+extract_and_write(input_file, output_file, start_line)
+print("8")
+
+#log_melting
+input_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/log/meltinglog.txt'
+start_line = 1
+extract_and_write(input_file, output_file, start_line)
+print("9")
+
+#log_para
+input_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/log/para_avoid_log.txt'
+start_line = 1
+extract_and_write(input_file, output_file, start_line)
+print("10")
+
+#log_gpsrunning1
+input_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/log/gpsrunning1log.txt'
+start_line = 1
+extract_and_write(input_file, output_file, start_line)
+print("11")
+
+#log_humandetect
+input_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/log/humandetectlog.txt'
+start_line = 1
+extract_and_write(input_file, output_file, start_line)
+print("12")
+
+#log_gpsrunning2
+input_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/log/gpsrunning2log.txt'
+start_line = 1
+extract_and_write(input_file, output_file, start_line)
+print("13")
+
+#log_photorunning
+input_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/log/photorunninglog.txt'
+start_line = 1
+extract_and_write(input_file, output_file, start_line)
+print("14")
