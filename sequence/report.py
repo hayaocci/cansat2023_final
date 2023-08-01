5b6cf4499d3dd446bf596f5a3f1d99828300fe05
Arashi1023 <nagasaki@kimura-lab.net>
2023-08-01 19:03:33 +0900

    report

--- sequence/report.py
+++ sequence/report.py
@@ -11,11 +11,33 @@ def extract_and_write(input_file, output_file, start_line, end_line=float('inf')
     except Exception as e:
         print(f"エラーが発生しました: {e}")
 
-# 使用例：抽出する行の範囲を3行目からファイルの最後までに指定
+def add_text_to_last_line(input_file, output_file, text_to_add):
+    try:
+        with open(input_file, 'r') as infile:
+            lines = infile.readlines()
+
+        with open(output_file, 'w') as outfile:
+            # 元のファイルの内容を新しいファイルに書き込む
+            outfile.writelines(lines)
+
+            # 「あいうえお」を追加して新しいファイルに書き込む
+            outfile.write(text_to_add + '\n')
+
+        print(f"「{text_to_add}」が {output_file} の最下行に追加されました。")
+    except Exception as e:
+        print(f"エラーが発生しました: {e}")
+
+
+
+#入力、出力するファイルを指定
 input_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/log/phaselog0109.txt'
-print("1")
 output_file = 'C:/Users/arass/OneDrive/ドキュメント/GitHub/cansat2023/kari/cansat2023/sequence/control_record_report.txt'
-print("1")
+#使用例：抽出する行の範囲を3行目からファイルの最後までに指定
 start_line = 3
 extract_and_write(input_file, output_file, start_line)
 print("1")
+
+# 使用例：元のファイルの内容を読み込み、最下行に「あいうえお」を追加して新しいファイルとして保存する
+text_to_add = "Time and position at which the control ended:"
+add_text_to_last_line(input_file, output_file, text_to_add)
+print("2")
