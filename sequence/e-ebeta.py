4c2710e2b189909e7432691232cc3b934f699582
Arashi1023 <nagasaki@kimura-lab.net>
2023-08-02 17:18:53 +0900

    a

--- sequence/e-ebeta.py
+++ sequence/e-ebeta.py
@@ -30,7 +30,7 @@ import beta_para_avoid as para_avoid
 import wgps_beta_photo_running as imgguide
 from math import sqrt
 #variable for log
-log_phase=other.filename('/home/dendenmushi/cansat2023/sequence/log/phaselog','txt')
+log_phase=other.filename('./log/phaselog','txt')
 log_report=other.filename('/home/dendenmushi/cansat2023/sequence/log/reportlog','txt')
 log_release=other.filename('/home/dendenmushi/cansat2023/sequence/log/releaselog','txt')
 log_landing=other.filename('/home/dendenmushi/cansat2023/sequence/log/landinglog','txt')
