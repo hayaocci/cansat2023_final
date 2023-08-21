d6ab860ef2d6f73452d23b2c3f742d9f41eb443c
hayaocci <7319079@ed.tus.ac.jp>
2023-08-21 17:23:26 +0900

    a

--- sequence/beta_para_avoid.py
+++ sequence/beta_para_avoid.py
@@ -150,8 +150,10 @@ def beta_para_avoid(red_area, angle, para_thd_avoid, check_count):
         赤色の面積
     angle : int
         重心からの角度
-    thd_red_area : float
+    para_thd_avoid : float
         赤色の面積がこれ以上大きいとパラシュートがあると判断する
+    check_count : int
+        何回確認するか
     '''
 
 
