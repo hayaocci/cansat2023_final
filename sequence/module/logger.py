fbdab18118b1594dbb6d85ecd6c3f52564a9f3f7
hayaocci <7319079@ed.tus.ac.jp>
2023-08-24 16:16:35 +0900

    ログをクラスを利用して作成するのを試している

---
+++ sequence/module/logger.py
@@ -0,0 +1,16 @@
+'''
+ログ関係のモジュール 作成途中0824
+'''
+
+import os
+import linecache
+
+class Logger:
+    def __init__(self):
+        self.path = ''
+
+    def save_log(self):
+        self.save = 
+
+release = Logger()
+release.path = 
